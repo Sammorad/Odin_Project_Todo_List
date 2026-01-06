b51ee3f9fa5add31100b4e0dab70a97384606f1a
dejiowoeye <50265769+Sammorad@users.noreply.github.com>
2026-01-06 16:03:16 +0100

    functions to add and view activities to the todo list created

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ Todos = class{
         this.date = date;
         this.status = status 
     }
-
+    //method to add activities to the list// 
     addTodo(){
         todoList.push(this)
 
@@ -15,16 +15,43 @@ Todos = class{
 
 }
 
+function addActivity(){
+    //function to add activities to the todo list 
+    let title = prompt("Input title: ");
+    let description = prompt("Input description for this todo: ");
+    let date = prompt("Input date in the format yyyy-mm-dd:");
+    let status  = prompt ("Is the activity conclluded 'true or false': ");
+    let todo1 = new Todos(title, description, date, status)
+    todo1.addTodo()
+    console.log(todoList)
+
+
+}
+
 function viewTodolist(){
-    //function to add to the list 
-    return console.log(todoList)
+    //function view specific activities.
+    let actToView = prompt("Enter Title of activity: ")
+    let act01 = todoList.find(item =>item.title == actToView)
+    console.log(act01)
+      
 
 }
-function to deleteItem(){
-    
+
+main = function(){
+    //main function controlling the todo list// 
+    let whatToDo = prompt("what will u do 'input todo:T', 'view todo: v': ")
+    if (whatToDo == "T"){
+        return addActivity()
+    }
+    else if (whatToDo == "v"){
+        return viewTodolist()
+    }
+
 
 }
-const eat = new Todos("eba","eaten for strength", "2026-03-10", true)
-eat.addTodo()
-viewTodolist()
+
+main()
+
+
+
 
